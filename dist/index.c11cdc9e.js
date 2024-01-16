cc955b875a7087748404b88540dab8ae1adb7806
phte1100 <phte1100@student.miun.se>
2024-01-16 10:03:31 +0100

    Skapat nya filer och ändrat struktur

---
+++ dist/index.c11cdc9e.js
@@ -0,0 +1,2 @@
+document.addEventListener("DOMContentLoaded",function(){var e=new Date().getFullYear();document.getElementById("currentYear").textContent=e});
+//# sourceMappingURL=index.c11cdc9e.js.map

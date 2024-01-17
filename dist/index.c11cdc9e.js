44f3a90f113c129754e30149f8fc34fe6b8fb205
phte1100 <phte1100@student.miun.se>
2024-01-17 15:03:39 +0100

    Fixade, .gitignore och tog bort dist och .parcel-cache. Ny mapp med bilder

--- dist/index.c11cdc9e.js
+++
@@ -1,2 +0,0 @@
-document.addEventListener("DOMContentLoaded",function(){var e=new Date().getFullYear();document.getElementById("currentYear").textContent=e});
-//# sourceMappingURL=index.c11cdc9e.js.map
